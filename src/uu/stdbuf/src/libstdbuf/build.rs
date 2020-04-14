ac9198598d699787145ae226274de62b6046623e
Roy Ivy III <rivy.dev@gmail.com>
2020-04-14 18:58:31 -0500

    wip ~ stdbuf/libstdbuf: direct 'NOT SUPPORTED' build feedback for windows

--- src/uu/stdbuf/src/libstdbuf/build.rs
+++ src/uu/stdbuf/src/libstdbuf/build.rs
@@ -1,7 +1,14 @@
 extern crate cpp_build;
 
+#[cfg(not(target_os = "windows"))]
 use cpp_build::Config;
 
+#[cfg(not(target_os = "windows"))]
 fn main() {
     Config::new().pic(true).build("src/libstdbuf.rs");
+}
+
+#[cfg(target_os = "windows")]
+fn main() {
+    panic!("target_os = \"windows\" is NOT SUPPORTED");
 }
